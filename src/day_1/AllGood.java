98872dd9cea29374994883385fa8bc90b4da107e
SereneKa <64221198+SereneKa@users.noreply.github.com>
2020-04-24 16:29:51 -0400

    Update AllGood.java

--- src/day_1/AllGood.java
+++ src/day_1/AllGood.java
@@ -5,6 +5,11 @@ public class AllGood {
     public static void main(String[] args) {
 
         System.out.println("my name is Serenefit add");
+        
+        // this line was changed from github
+        
+        
+        
     }
 
 
